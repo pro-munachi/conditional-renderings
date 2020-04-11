c9830188f15ece4f57542a537340c51ae06a1a77
madu munachi <madustanley1@gmail.com>
2020-04-11 19:05:40 +0100

    render

---
+++ App (2).js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { 
+      loading: false,
+    character: {}
+    }
+  }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch("https://swapi.co/api/people/1")
+    .then(response => response.json())
+    .then(data => {
+      this.setState(
+        {
+          loading: false,
+          character: data
+        }
+      )
+    })
+  }
+  
+  render() {
+  let text = this.state.loading ? "loading......" : this.state.character.name
+    return (
+      <div className='App'>
+    {text}
+      </div>
+    )
+  }
+}
+
+export default App;
